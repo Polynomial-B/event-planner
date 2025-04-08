6bb759bd84f2c6617d6acc010cb9854661e4a422
Matt <mphlamb@gmail.com>
2025-04-08 14:31:36 +0200

    initial commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotFound() {
+	return <main>404 - Page not found :(</main>;
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer() {
+	return <footer>ML {new Date().getFullYear()}©</footer>;
+}
