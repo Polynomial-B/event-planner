7daef2fbf50bb931180e66dc230096b357044ebd
Matt <mphlamb@gmail.com>
2025-04-09 17:28:51 +0200

    updated

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,5 +1,15 @@
+import Title from "@/components/Title";
+import { EventsPageProps } from "@/lib/types";
 import React from "react";
 
-export default function EventsPage() {
-	return <main>EventsPage</main>;
+export default function EventsPage({ params }: EventsPageProps) {
+	const city = params.city;
+	const capitalisedCity = city.charAt(0).toUpperCase() + city.slice(1);
+	return (
+		<main className="flex flex-col items-center py-24 px-20 min-h-[110vh]">
+			<Title>
+				{city === "all" ? "All Events" : `Events in ${capitalisedCity}`}
+			</Title>
+		</main>
+	);
 }
