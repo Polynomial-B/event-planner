a0bd62446b121ab7760d5ea107f771ec37be51ad
Matt <mphlamb@gmail.com>
2025-05-27 06:56:09 +0200

    redirect added for events path

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req: Request) {
+  return NextResponse.redirect(new URL("/events/all", req.url));
+}
+export const config = {
+  matcher: ["/events", "/evetns"],
+};
